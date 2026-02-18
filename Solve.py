500bc050cf5f39d8572a9a1a05b3b16a81645dfc
Boitumelo Rachoshi <boracjhb025@student.wethinkcode.co.za>
2026-02-18 15:04:08 +0200

    solutions folder

--- Solve.py
+++ Solve.py
@@ -6,35 +6,44 @@ import sys
 # ==========
 def even_sum(n):
     pass
+
 # Functions
 # ==========
 # TODO : Question 2
 # ==========
 def fahre_to_cels(temp):
     pass
+
 # Algorithms
 # ==========
 # TODO : Question 3
 # ==========
 def is_palindrome(str):
     pass
+
 # Command line Commands
 # ==========
 # TODO : Question 4
 # ==========
 def repeat_script(string=None,n=None):
     pass
+
 # Data Structures
 # ==========
 # TODO : Question 5
-# ==========
+def find_the_cheapest(catalogue={}):
+    pass
 
+# ==========
 # Data Manipulation
 # ==========
 # TODO : Question 6
 # ==========
-
+def email_refiner(emails=[]):
+    pass
 #  String Formatting
 # ==========
 # TODO : Question 7
 # ==========
+def receipt_formatter(item,price):
+    pass
